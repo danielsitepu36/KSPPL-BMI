549a7054a44352f4035f66b23d52298f3c261ea5
danielsitepu36 <danielsitepu96@gmail.com>
2020-10-05 13:37:31 +0700

    text-optimization

--- bmi-functions/functions/index.js
+++ bmi-functions/functions/index.js
@@ -6,28 +6,50 @@ const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+// exports.helloWorld = functions.https.onRequest((req, res) => {
+// });
+
 const countBMI = (req, res) => {
   const userData = {
     age: req.body.age,
     weight: req.body.weight,
     height: req.body.height,
   };
-  if (userData.weight == null || userData.height == null) {
+
+  const result = {
+    value: 0,
+    category: "Normal",
+  };
+  if (
+    userData.age == null ||
+    userData.weight == null ||
+    userData.height == null
+  ) {
     return res.status(400).json({ error: "Data is not complete" });
   }
 
   BMIWeight = parseFloat(userData.weight);
   BMIHeight = parseFloat(userData.height) / 100.0;
-  if (!BMIHeight > 0 || !BMIWeight > 0) {
+  if (!userData.age > 0 || !BMIHeight > 0 || !BMIWeight > 0) {
     return res
       .status(400)
       .json({ error: "Data must be a number & bigger than 0" });
   }
 
-  BMI = BMIWeight / (BMIHeight * BMIHeight);
+  result.value = BMIWeight / (BMIHeight * BMIHeight);
 
-  return res.json({ BMI });
+  if (result.value < 18.5) {
+    result.category = "Underweight";
+  } else if (result.value < 25) {
+    result.category = "Normal";
+  } else if (result.value < 30) {
+    result.category = "Overweight";
+  } else {
+    result.category = "Obese";
+  }
+
+  return res.json({ result });
 };
 
 app.post("/bmi", countBMI);
